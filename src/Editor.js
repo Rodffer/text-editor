8e9f5b9f0b3e35f3f7090e27777faf5cb17b3da2
Rayron Rodffer <r.rodffer@gmail.com>
2020-06-17 15:05:33 -0300

    text editor

--- src/Editor.js
+++ src/Editor.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { AiOutlineFileText } from 'react-icons/ai';
 import CKEditor from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import { BrowserRouter } from 'react-router-dom';
 import GlobalStyle from './styles/global';
 import { Container } from './styles/stylesEditor';
 
 function Editor() {
   return (
     <>
+      <BrowserRouter basename="text-editor" />
       <GlobalStyle />
       <Container>
         <h1>
